b1b3acef3109142c5861c683a4297c68d1f1726a
Paul Tader <paul.tader@linuxscope.com>
2022-09-07 20:29:21 -0500

    wip

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "config" {
   value = {
     bucket         = aws_s3_bucket.s3_bucket.bucket
     region         = data.aws_region.current.name
-    role_arn       = aws_iam_role.arn
+    role_arn       = aws_iam_role.iam_role.arn
     dynamodb_table = aws_dynamodb_table.dynamodb_table.name
   }
 }
